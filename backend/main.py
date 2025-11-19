d65c5e06f9873c00349454b7573ef47eef3a40c4
Opeyemi <thomasopeyemi1@gmail.com>
2025-11-19 10:49:03 +0100

    Further Commit

--- backend/main.py
+++ backend/main.py
@@ -29,18 +29,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[
-#         "https://my-portfolio.onrender.com",
-#         "http://localhost:5173",
-#     ],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-
 # DB dependency
 def get_db():
     db = SessionLocal()
